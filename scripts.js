8bfc84d0cd936331d122b98edb73e4806fb79633
Justin <vien_justin@hotmail.com>
2022-01-29 17:40:46 -0500

    some work on slider

--- scripts.js
+++ scripts.js
@@ -5,16 +5,45 @@ let titleAnimation = anime({
 })
 
 
-const projects = [];
+const projects = [
 
-const project = {
+];
+
+const projectOne = {
     name: "JS Hero",
     description: "A simple javascript quiz game.",
-    image: "./assets/JS-Hero.png"
+    image: "./assets/JS-Hero.png",
+    source: "https://github.com/justinv416/JS-Hero",
+    liveLink: "https://jshero.netlify.app"
+}
+
+const projectTwo = {
+    name: "Art Unlimited",
+    description: "A art generator.",
+    image: "./assets/Art-Unlimited.png",
+    source: "https://github.com/justinv416/Art-Unlimited",
+    liveLink: "https://artunlimited.netlify.app"
 }
 
 const displayProject = () => {
-    document.querySelector('.projectHeading').textContent = project.name;
+    document.querySelector('.projectHeading').textContent = projectOne.name;
+    document.querySelector('.projectDescription').textContent = projectOne.description;
+    document.querySelector('.projectImage').src = projectOne.image;
 }
 
-displayProject()
+//Controls for the image carousel.
+const sliderControls = () => {
+    document.querySelector('.fa-chevron-left').addEventListener('click', () => {
+        console.log('clicked')
+    });
+    document.querySelector('.fa-chevron-right').addEventListener('click', () => {
+        console.log('clicked')
+    });
+}
+
+const init = () => {
+    displayProject();
+    sliderControls();
+}
+
+init();
