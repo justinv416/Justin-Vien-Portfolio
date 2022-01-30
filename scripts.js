0765d0cc001aaf479353dd7b33a32014711256b2
Justin <vien_justin@hotmail.com>
2022-01-29 19:56:09 -0500

    more work on project slider functionality

--- scripts.js
+++ scripts.js
@@ -1,49 +1,75 @@
 //Anime.js title animation
-let titleAnimation = anime({
-    targets: 'h1.from-to-values',
-    opacity: [0, 1],
-})
+// let titleAnimation = anime({
+//     targets: 'h1',
+//     translateY: -100
+// })
 
 
 const projects = [
-
+    {
+        name: "JS-Hero",
+        description: `A quiz game that features a set of 14 javascript trivia questions.
+                    Upon a correct answer users will see a attack animation indicating the enemey is defeated,
+                    and a respective take damage animation if the user gets the question wrong.
+                    I designed the UI with inspiration from old final fantasy RPGs. `,
+        image: "./assets/JS-Hero.png",
+        stack: "This project was built with: HTML5, CSS3, jQuery, and Howler.js.",
+        source: "https://github.com/justinv416/JS-Hero",
+        liveLink: "https://jshero.netlify.app"
+    },
+    {
+        name: "Art Unlimited",
+        description: `A website that utilizes the Art Institute of Chicago's API which allows users to to generate 
+                    random or search for artworks for inspiration or their viewing pleasure. This app features API loading animations, and an opening 
+                    animation created with Green Sock Animation Plugin and pagination for the search results.`,
+        image: "./assets/Art-Unlimited.png",
+        stack: "This project was built with: HTML5, CSS3, jQuery, and GSAP",
+        source: "https://github.com/justinv416/Art-Unlimited",
+        liveLink: "https://artunlimited.netlify.app"
+    },
+    {
+        name: "Reel Good Films",
+        description: `Reel Good Films is a website is a pair-programming project that I built with Mark Duffy. 
+                    This website utilizes The Movie Database API that allows users to search for movies from their database or
+                    discover popular movies based on a selected year or genre.`,
+        image: "./assets/Reel-Good-Films.png",
+        stack: "This project was built with: HTML5, CSS3, Vanilla JS, and ES6.",
+        source: "https://github.com/MarkAndJustin/ReelGoodFilms",
+        liveLink: "https://reelgoodfilms.netlify.app/"
+    },
 ];
 
-const projectOne = {
-    name: "JS Hero",
-    description: "A simple javascript quiz game.",
-    image: "./assets/JS-Hero.png",
-    source: "https://github.com/justinv416/JS-Hero",
-    liveLink: "https://jshero.netlify.app"
-}
 
-const projectTwo = {
-    name: "Art Unlimited",
-    description: "A art generator.",
-    image: "./assets/Art-Unlimited.png",
-    source: "https://github.com/justinv416/Art-Unlimited",
-    liveLink: "https://artunlimited.netlify.app"
-}
 
-const displayProject = () => {
-    document.querySelector('.projectHeading').textContent = projectOne.name;
-    document.querySelector('.projectDescription').textContent = projectOne.description;
-    document.querySelector('.projectImage').src = projectOne.image;
+const displayProject = (count) => {
+    document.querySelector('.projectHeading').textContent = projects[count].name;
+    document.querySelector('.projectDescription').textContent = projects[count].description;
+    document.querySelector('.projectImage').src = projects[count].image;
+    document.querySelector('.projectStack').textContent = projects[count].stack;
+    document.querySelector('.sourceLink').href = projects[count].source;
+    document.querySelector('.liveLink').href = projects[count].liveLink;
 }
 
 //Controls for the image carousel.
-const sliderControls = () => {
+const sliderControls = (count) => {
     document.querySelector('.fa-chevron-left').addEventListener('click', () => {
-        console.log('clicked')
+        if(count === 0){
+            count === 0;
+        } else {
+            count--;
+        }
+        displayProject(count);
     });
     document.querySelector('.fa-chevron-right').addEventListener('click', () => {
-        console.log('clicked')
+        count++;
+        console.log(count)
+        displayProject(count)
     });
 }
 
 const init = () => {
-    displayProject();
-    sliderControls();
+    // displayProject();
+    sliderControls(0)
 }
 
 init();
